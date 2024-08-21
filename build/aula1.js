e5844a10acfa0d5ca9c82678840a9e0a4d392cfc
RodrigoDev27 <rodryygo460@gmail.com>
2024-08-21 19:00:01 -0300

    first commit

---
+++ build/aula1.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "joao";
+console.log("Bem vindo " + nome);
+console.log("Apredendo js");
+console.log("De novo 2");
---
+++ src/aula1.ts
@@ -0,0 +1,9 @@
+const nome = "joao";
+console.log("Bem vindo " + nome);
+console.log("Apredendo js");
+console.log("De novo 2");
+
+
+
+
+
